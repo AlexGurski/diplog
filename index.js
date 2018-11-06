a520612384c751749a71fffe0a3046decdca8352
etosolo322 <etosolo322@yandex.ru>
2018-11-06 14:24:19 +0300

    make it better

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use( bodyParser.urlencoded( {extended:true} ) );
 app.use( bodyParser.json() )
 app.use('/public', express.static('public'));
 
-let  desert= require("./public/json/desert.js");
+let desert= require("./public/json/desert.js");
 let menuPizza = require("./public/json/menuPizza.js");
 let menuHot = require("./public/json/menuHot.js");
 let menuCold = require("./public/json/menuCold.js");
@@ -15,8 +15,13 @@ let cocktail= require("./public/json/cocktail.js");
 let pivo= require("./public/json/pivo.js");
 let vodka= require("./public/json/vodka.js");
 let tea= require("./public/json/tea.js");
-let allMenuWith = menuPizza.concat(menuHot,menuCold, menuGarnirs,cocktail,desert);
-let allMenuWithout = pivo.concat(vodka,tea);
+let sokmorozh= require("./public/json/sokmorozh.js");
+let supzavtrak= require("./public/json/supzavtrak.js");
+let sandwblinch= require("./public/json/sandwblinch.js");
+let salat= require("./public/json/salat.js");
+
+let allMenuWith = menuPizza.concat(salat, sandwblinch, supzavtrak, menuHot,menuCold, menuGarnirs,cocktail,desert);
+let allMenuWithout = pivo.concat(vodka,tea,sokmorozh);
 app.get('/',(req, res) => {
   res.render('index.ejs');
 })
---
+++ public/json/salat.js
@@ -0,0 +1,11 @@
+module.exports =  [
+  {
+    id:'salatavenju',
+    name:'Салат "Авеню"',
+    gram:'1/150',
+    price:'2р 70к',
+    kind:'salat',
+    discription:'цыплята, яйцо, арахис, сыр, чернослив, апельсин, майонез'
+  },
+
+]
