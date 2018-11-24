c246897ff0cca2bfdc9fae6e163573170d0be1ee
etosolo322 <etosolo322@yandex.ru>
2018-11-24 23:51:14 +0300

    make it better

--- public/js/preloader.js
+++ public/js/preloader.js
@@ -1,6 +1,7 @@
 
 //window.addEventListener('load', function() {
-window.onload = function(){let preload=document.getElementById('preloader');
+window.onload = function(){
+  var preload=document.getElementById('preloader');
 preload.style.display = 'none';
 console.log('sdfdf');
 }
