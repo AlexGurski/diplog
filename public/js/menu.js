7b849f7a782ea2610e0d056fccbcb8d291c5e1a7
etosolo322 <etosolo322@yandex.ru>
2018-11-25 00:00:52 +0300

    make it better

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,15 +1,15 @@
 async function getElementMenu(){
-    var responseMenu = await fetch('/allMenu')
-  var products = await responseMenu.json();
+    let responseMenu = await fetch('/allMenu')
+    let products = await responseMenu.json();
 
     await   new Promise((resolve, reject) => setTimeout(resolve, 0));
     await   create(products);
     console.log(products)
-    for (var i = 0; i < document.getElementsByClassName('headerItemMenu').length;i++ ){
+    for (let i = 0; i < document.getElementsByClassName('headerItemMenu').length;i++ ){
     document.getElementsByClassName('headerItemMenu')[i].onclick = ()=>{
-    var idClassName = document.getElementsByClassName('headerItemMenu')[i].id
+    let idClassName = document.getElementsByClassName('headerItemMenu')[i].id
 
-    for (var i=0;i < products.length;i++){
+    for (let i=0;i < products.length;i++){
       if (products[i]._id === idClassName){
         document.getElementById('enterKind').innerHTML = products[i].name.toUpperCase()
       }
@@ -22,15 +22,15 @@ async function getElementMenu(){
 
 function filter (name,kind){
   //console.log(name, kind);
-  for (var i = 0; i < document.getElementsByClassName('ItemsCenter').length;i++ ){
+  for (let i = 0; i < document.getElementsByClassName('ItemsCenter').length;i++ ){
     document.getElementsByClassName('ItemsCenter')[i].style.display='none';
   }
-  for (var i = 0; i < document.getElementsByClassName('menuItemWith').length;i++ ){
+  for (let i = 0; i < document.getElementsByClassName('menuItemWith').length;i++ ){
   document.getElementsByClassName('menuItemWith')[i].style.display='none';
 
 }
 if (kind !==undefined){
-      for (var i = 0; i < document.getElementsByClassName(name).length;i++ ){
+      for (let i = 0; i < document.getElementsByClassName(name).length;i++ ){
       document.getElementById('centerMain').style.transform='scale(0.1)';
     setTimeout(()=>{
     document.getElementsByClassName(name)[i].style.display='block';
@@ -38,7 +38,7 @@ if (kind !==undefined){
   },300)
   }
 }
-else {  for (var i = 0; i < document.getElementsByClassName(name).length;i++ ){
+else {  for (let i = 0; i < document.getElementsByClassName(name).length;i++ ){
 
  document.getElementById('centerMain').style.opacity=0;
  setTimeout(()=>{
@@ -49,27 +49,27 @@ else {  for (var i = 0; i < document.getElementsByClassName(name).length;i++ ){
 
 }
 async function getMenuWith(){
-    var responseMenu = await fetch('/menuWith');
-    var responseMenuWithout = await fetch('/menuWithout');
+    let responseMenu = await fetch('/menuWith');
+    let responseMenuWithout = await fetch('/menuWithout');
 //console.log(responseMenu)
-    var productsWithout = await responseMenuWithout.json();
-    var products = await responseMenu.json();
+    let productsWithout = await responseMenuWithout.json();
+    let products = await responseMenu.json();
    console.log(products);
     console.log(productsWithout);
     await   new Promise((resolve, reject) => setTimeout(resolve, 0));
     await   createMenuPretty(products);
     await   createMenuPrettyWithout(productsWithout);
-    var items = products.concat(productsWithout);
+    let items = products.concat(productsWithout);
 //    let rezult = searchMenu(products);
   //  console.log(rezult);
     return  await items;
 }
 
 async function searchMenu(){
-var discriptionForSearch = [];
-var responseMenu =  await fetch('/menuWith');
-varitems = await  responseMenu.json();
-  for (var i=0;i < items.length; i++){
+  let discriptionForSearch = [];
+  let responseMenu =  await fetch('/menuWith');
+  let items = await  responseMenu.json();
+  for (let i=0;i < items.length; i++){
     if((items[i].discription!==undefined) && (items[i].discription.includes(document.getElementById('searchTextMenu').value)))
    {
       discriptionForSearch.push(items[i]);
@@ -81,13 +81,13 @@ filterOne(discriptionForSearch);
 
 function filterOne(item){
         console.log(item);
-        for (var i = 0; i < document.getElementsByClassName('ItemsCenter').length;i++ ){
+        for (let i = 0; i < document.getElementsByClassName('ItemsCenter').length;i++ ){
           document.getElementsByClassName('ItemsCenter')[i].style.display='none';
         }
-        for (var i = 0; i < document.getElementsByClassName('menuItemWith').length;i++ ){
+        for (let i = 0; i < document.getElementsByClassName('menuItemWith').length;i++ ){
         document.getElementsByClassName('menuItemWith')[i].style.display='none';
       }
-       for (var i=0;i<item.length;i++){
+       for (let i=0;i<item.length;i++){
          document.getElementById('00'+item[i]._id).style.display='block';
        }
 }
@@ -122,8 +122,8 @@ function arrayUnique(massivKind){
 function createMenuPrettyWithout(items){
 //  console.log(items);
 
-var massivKind = [];
-for (var i=0;i<items.length;i++){
+let massivKind = [];
+for (let i=0;i<items.length;i++){
   massivKind[i] =  items[i].kind;
 }
 
@@ -131,7 +131,7 @@ for (var i=0;i<items.length;i++){
 massivKind = arrayUnique(massivKind);
 //console.log(massivKind);
 
-for (var i=0;i<massivKind.length;i++){
+for (let i=0;i<massivKind.length;i++){
   const  div = document.createElement("div");
   div.className = 'menuItemWith '+massivKind[i];
   div.id = '00'+massivKind[i];
